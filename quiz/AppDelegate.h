0964f7606bb0ad057a65bf2882326e4faecd842a
Yanze LIU <yanzeliu.france@gmail.com>
2016-03-17 11:24:03 -0700

    Initial Commit

---
+++ quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  quiz
+//
+//  Created by yanze on 3/17/16.
+//  Copyright © 2016 yanze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
